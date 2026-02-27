24ede22aaaacd7a517b80f083a17758b501c3d44
Sorin Radu <sorin.radu@gmail.com>
2026-02-28 00:48:16 +0200

    feat: bootstrap Spring Boot project (web, jpa, security, flyway, postgres)

---
+++ bookwise-api/src/main/java/com/bookwise/bookwise_api/BookwiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.bookwise.bookwise_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwiseApiApplication.class, args);
+	}
+
+}
